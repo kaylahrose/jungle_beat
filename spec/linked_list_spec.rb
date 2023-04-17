7a76834553d9f4d8c9abad4a208f792e39c847a1
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-04-17 10:28:16 -0600

    Test: linked list

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -5,15 +5,20 @@ RSpec.describe LinkedList do
   it 'exists and has attributes' do
     list = LinkedList.new
 
-    expect(list).to be_a(List)
+    expect(list).to be_a(LinkedList)
     expect(list.head).to eq(nil)
+  end
 
-    list.append('doop')
-    
-    expect(list.head).to be_a(Node)
-    expect(list.head.data).to eq('doop')
-    expect(list.head.next_node).to eq(nil)
-    expect(list.count).to eq(1)
-    expect(list.to_string).to eq('doop')
+  context 'instance methods' do
+    it 'appends a node to a list' do
+      list = LinkedList.new
+      list.append('doop')
+
+      expect(list.head).to be_a(Node)
+      expect(list.head.data).to eq('doop')
+      expect(list.head.next_node).to eq(nil)
+      expect(list.count).to eq(1)
+      expect(list.to_string).to eq('doop') 
+    end
   end
 end
