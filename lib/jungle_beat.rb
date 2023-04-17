b1c3dcf116a23764fd31e5666c217ec72dcddfaf
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-04-17 14:27:11 -0600

    Feat: append jungle beat

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,7 +1,13 @@
 class JungleBeat
   attr_reader :list
-  
+
   def initialize
     @list = LinkedList.new
+  end
+
+  def append(data)
+    data.split(' ').each do |element|
+      @list.append(element) 
+    end
   end
 end
