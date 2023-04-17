7287a7857dfc3df2476a8f4e639b13858710d925
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-04-17 11:54:28 -0600

    Test: list find

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -52,6 +52,17 @@ RSpec.describe LinkedList do
         expect(list.to_string).to eq("dop woo plop suu")
         expect(list.count).to eq(4)
       end
+
+      it '#find' do
+        list = LinkedList.new
+        list.append("deep")
+        list.append("woo")
+        list.append("shi")
+        list.append("shu")
+        list.append("blop")
+        expect(list.find(2, 1)).to eq('shi')
+        expect(list.find(1, 3)).to eq('woo shi shu')
+      end
     end
   end
 end
