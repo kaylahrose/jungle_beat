ac72f6c5be8eb8d766352bf5776a780f17cb1c5a
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-04-17 13:19:14 -0600

    Test: list include

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -69,6 +69,18 @@ RSpec.describe LinkedList do
         expect(list.find(2, 1)).to eq('shi')
         expect(list.find(1, 3)).to eq('woo shi shu')
       end
+
+      it '#include' do
+        list = LinkedList.new
+        list.append("deep")
+        list.append("woo")
+        list.append("shi")
+        list.append("shu")
+        list.append("blop") 
+
+        expect(list.includes?("deep")).to eq(true)
+        expect(list.includes?("dep")).to eq(false)
+      end
     end
   end
 end
